8bf11b81f6c8ba8570079e5ace60e6d8c884bab7
ciett <teic0701@gmail.com>
2024-01-17 20:39:42 +0700

    chang file bai1 java

--- src/btvnbuoi2/bai1.java
+++ src/btvnbuoi2/bai1.java
@@ -1,5 +1,54 @@
 package btvnbuoi2;
 
-public class bai1 {
 
+	import java.util.Scanner;
+	
+	public class bai1 {
+		
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+// //1
+//		System.out.println("nhap vao chu so dau tien:");
+//		int a = scanner.nextInt();
+//		System.out.println("nhap vao chu so thu 1:");
+//		int b = scanner.nextInt();
+//		System.out.println("nhap vao chu so thu 2:");
+//		int c = scanner.nextInt();
+//	int max = a;
+//	if (a <b) {
+//		max = b;
+//	}
+//	if (c>a) {
+//		max = c;
+//	}
+//	System.out.println("chu so lon nhat la: " + max);
+//	
+//
+//	//2
+//		System.out.println("Nhap vao so diem can xet: ");
+//		float x = scanner.nextFloat();
+//		if (x>8) {
+//			System.out.println("gioi");}
+//			else if (x> 6.5) {
+//				System.out.println("kha");}
+//			else if (x>5) {
+//				System.out.println("trung binh");}
+//				else {
+//					System.out.println("kem'");}
+//	
+//	}
+//}
+	// 3
+		System.out.println("nhap vao` 1 so: ");
+		float y = scanner.nextFloat();
+		if (y>0) {
+			System.out.println("Positive");}
+			else if (y <0) {
+				System.out.println("Negative");
+			}
+				else {
+					System.out.println("gia tri = 0");}
+			
+	}
 }
+	
